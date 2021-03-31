ad957b9b9e2147a6322957f2fa3bf5ce2f4b211d
Anurag Kumar Singh <52702259+anurag3301@users.noreply.github.com>
2021-03-31 21:57:35 +0530

    Added Discord Installer

---
+++ scripts/discord_installer.sh
@@ -0,0 +1,27 @@
+if [ -d ~/.program ]; then
+    echo "~/.program directory exits, cd into it"
+    cd ~/.program
+else
+    echo "~/.program directory does not exits, creating the directory and cd into it"
+    mkdir ~/.program
+    cd ~/.program
+fi
+
+wget -O discord.gz https://discord.com/api/download\?platform\=linux\&format\=tar.gz
+wait
+tar xvf discord.gz
+wait
+rm discord.gz
+wait
+cd ~/.local/share/applications
+
+echo "[Desktop Entry]
+Name=Discord
+Exec=$HOME/.program/Discord/Discord
+Icon=$HOME/.program/Discord/discord.png
+Terminal=false
+Type=Application
+Categories=Network;InstantMessaging;
+X-GNOME-UsesNotifications=true
+" >> discord.desktop
+
