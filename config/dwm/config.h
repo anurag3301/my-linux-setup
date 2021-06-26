8507a9d172aa33f0634da01a1f550c08b8a5311f
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-06-26 09:38:24 +0530

    Added strict rules for application to spawn

--- config/dwm/config.h
+++ config/dwm/config.h
@@ -28,8 +28,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Brave-browser",  NULL, NULL,       0,   	    0,            0 },
+	{ "discord",  NULL,       NULL,       1 << 1,       0,            0 },
+	{ "kitty",    NULL,       NULL,       0,            0,            1 },
+	{ "Spotify",  NULL,       NULL,       1 << 3,       0,            1 },
+
 };
 
 /* layout(s) */
