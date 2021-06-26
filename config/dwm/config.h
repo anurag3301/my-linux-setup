e9063f87c2b346f2cb0785830f2e2147fc54bdf8
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-06-26 18:34:57 +0530

    Added strict rules for telgram

--- config/dwm/config.h
+++ config/dwm/config.h
@@ -32,6 +32,7 @@ static const Rule rules[] = {
 	{ "discord",  NULL,       NULL,       1 << 1,       0,            0 },
 	{ "kitty",    NULL,       NULL,       0,            0,            1 },
 	{ "Spotify",  NULL,       NULL,       1 << 3,       0,            1 },
+	{ "TelegramDesktop",NULL, NULL,       1 << 4,       0,            1 },
 
 };
 
