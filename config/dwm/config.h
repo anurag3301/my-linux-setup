00353ac635109feb37335981feee406c6e8b1641
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-07-02 14:54:21 +0530

    Updated colourscheme of dwm

--- config/dwm/config.h
+++ config/dwm/config.h
@@ -12,7 +12,7 @@ static const char col_gray1[]       = "#282a36";
 static const char col_gray2[]       = "#282a36";
 static const char col_gray3[]       = "#96b5b4";
 static const char col_gray4[]       = "#d7d7d7";
-static const char col_cyan[]        = "#4d4d4d";
+static const char col_cyan[]        = "#282a36";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
