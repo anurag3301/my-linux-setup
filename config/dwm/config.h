234e68f7278ff4e694cb00106a1aa47f076f116f
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-07-03 16:27:31 +0530

    Added barpadding dwm patch

--- config/dwm/config.h
+++ config/dwm/config.h
@@ -6,6 +6,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 0;       /* vertical padding of bar */
+static const int sidepad            = 0;       /* horizontal padding of bar */
 static const char *fonts[]          = {"JetBrainsMono Nerd Font:size14"};
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
 static const char col_gray1[]       = "#282a36";
