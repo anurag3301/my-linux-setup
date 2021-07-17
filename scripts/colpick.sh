d602de7e52b4f7e6935d55be6d11f31ef3fffa36
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-07-17 14:09:12 +0530

    Added script to pick colour

---
+++ scripts/colpick.sh
@@ -0,0 +1,3 @@
+var=$(colorpicker --short --one-shot | awk -F 'd' '{ print tolower($1)}');
+echo "$var";
+echo "$var" | xclip -selection clipboard; notify-send  " " "<span color='$var' font='26px'><b>$var</b></span><span font='20'> Copied to clipboard </span>";
