8ec0ef8cc7a131ca1ac47a2fd15133d5065cfff9
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-07-27 11:29:03 +0530

    Added nvimtree.lua

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -2,9 +2,30 @@ return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   use 'nvim-lua/plenary.nvim'
   use {'p00f/cphelper.nvim', rocks = 'http', requires = 'nvim-lua/plenary.nvim'}
-  use 'marko-cerovac/material.nvim'
+
   use {
-  'hoob3rt/lualine.nvim',
-  requires = {'kyazdani42/nvim-web-devicons', opt = true}
+    'marko-cerovac/material.nvim',
   }
+
+  use {
+    'hoob3rt/lualine.nvim',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true}
+  }
+  
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    event = "BufRead",
+    config = function()
+        require("plug_config.treesitter")
+    end,
+  }
+
+  use {
+      "kyazdani42/nvim-tree.lua",
+      config = function()
+          require("plug_config.tree")
+      end,
+      after = "nvim-web-devicons",
+    }
+
 end)
