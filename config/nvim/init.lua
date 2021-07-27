6c095cc9aa7c15582085b7cb76aa5b3d92bee49d
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-07-27 10:29:39 +0530

    Added matierial.nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -11,4 +11,19 @@ end
 
 -- include plugins
 require('plugins')
-require('lualine').setup()
+require('lualine').setup{
+	  options = {
+      -- ... your lualine config
+      theme = 'material-nvim'
+      -- ... your lualine config
+  }
+}
+vim.g.material_style = 'deep ocean'
+vim.g.material_italic_comments = true
+vim.g.material_italic_keywords = true
+vim.g.material_italic_functions = true
+vim.g.material_italic_variables = false
+vim.g.material_contrast = true
+vim.g.material_borders = false
+vim.g.material_disable_background = false
+require('material').set()
