74c374b0220e0f4a11a0806d63fc36e4f3f5123c
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-07-28 10:10:20 +0530

    Added cphelper config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -19,11 +19,15 @@ require('lualine').setup{
   }
 }
 
+-- CpHelper config
+vim.g.cphdir = '/home/anurag/problems'
+
+
 vim.g.material_style = 'deep ocean'
 vim.g.material_italic_comments = true
 vim.g.material_italic_keywords = true
 vim.g.material_italic_functions = true
-vim.g.material_italic_variables = false
+vim.g.material_italic_variables = true
 vim.g.material_contrast = false
 vim.g.material_borders = false
 vim.g.material_disable_background = false
