86c3b595128928f93fdcf138b30796dcb96b0c08
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-08-02 14:33:08 +0530

    Toggleterm config in a separeate file

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -18,6 +18,7 @@ require('autopairs')
 require("colors")
 require("dashboard")
 require("general")
+require("toggleTerm")
 
 -- include presence and its config
 require("presence"):setup({
@@ -32,22 +33,6 @@ require('lualine').setup{
 	  options = {
       theme = 'material-nvim'
   }
-}
-
--- include toggleterm
-require("toggleterm").setup{
-  size = 15,
-  open_mapping = '<F1>',
-  hide_numbers = true,
-  shade_filetypes = {},
-  shade_terminals = true,
-  shading_factor = 1,
-  start_in_insert = true,
-  insert_mappings = true,
-  persist_size = true,
-  direction = 'horizontal',
-  close_on_exit = true,
-  shell = vim.o.shell, -- change the default shell
 }
 
 -- CpHelper config
