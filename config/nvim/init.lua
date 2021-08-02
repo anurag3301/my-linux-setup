f777ccbb1fd9fe412f3cf4ca6981b1e6248284da
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-08-02 14:36:11 +0530

    presence config in a separeate file

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -19,14 +19,7 @@ require("colors")
 require("dashboard")
 require("general")
 require("toggleTerm")
-
--- include presence and its config
-require("presence"):setup({
-    enable_line_number = true,
-    main_image = "file",
-    neovim_image_text = "The gods editor",
-    debounce_timeout = 10,
-})
+require("presence-nvim")
 
 -- include lualine and its config
 require('lualine').setup{
---
+++ config/nvim/lua/presence-nvim.lua
@@ -0,0 +1,7 @@
+-- include presence and its config
+require("presence"):setup({
+    enable_line_number = true,
+    main_image = "file",
+    neovim_image_text = "The gods editor",
+    debounce_timeout = 10,
+})
