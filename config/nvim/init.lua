3e49809c76dd478912b25436380282b6a474cba9
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-08-02 14:14:31 +0530

    general config in a separeate file

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -62,41 +62,4 @@ require("colors")
 
 require("dashboard")
 
--- General config
-vim.opt['number'] = true
-vim.opt['ruler'] = true
-vim.opt['splitright'] = true
-vim.opt['splitbelow'] = true
-vim.opt['smarttab'] = true
-vim.opt['expandtab'] = true
-vim.opt['smartindent'] = true
-vim.opt['autoindent'] = true
-vim.opt['tabstop'] = 2
-vim.opt['shiftwidth'] = 2
-vim.opt['autoread'] = true
-vim.opt['title'] = true
-vim.opt['termguicolors'] = true
-vim.opt['hlsearch'] = true
-vim.opt['hidden'] = true
-vim.opt['encoding'] = 'utf-8'
-vim.opt['fileencoding'] = 'utf-8'
-vim.opt['showmode'] = false
-vim.opt['clipboard'] = 'unnamedplus'
-vim.opt['swapfile'] = false
-
--- Persistent undo
--- mkdir $HOME/.vim/undo
-vim.opt['undofile'] = true
-vim.opt['undodir'] = '/home/anurag/.cache/nvim/undo'
-vim.opt['undolevels'] = 1000
-vim.opt['undoreload'] = 10000
-
--- Stop comments on newline
-vim.cmd([[autocmd BufWinEnter * :set formatoptions-=c formatoptions-=r formatoptions-=o]])
-
--- Auto remove trailing space
-vim.cmd([[autocmd BufWritePre * %s/\s\+$//e]])
-
--- Disable lualine on Nvim-tree
-vim.cmd [[au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree" | set laststatus=0 | else | set laststatus=2 | endif]]
-
+require("general")
