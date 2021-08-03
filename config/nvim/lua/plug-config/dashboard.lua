a29cf0ca4c482177226d8e8751446ecbc416ad2e
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-08-03 11:52:00 +0530

    Updated Dashboard config

--- config/nvim/lua/plug-config/dashboard.lua
+++ config/nvim/lua/plug-config/dashboard.lua
@@ -17,3 +17,19 @@ vim.g.dashboard_custom_header = {
     "                                                              "
 }
 vim.cmd([[highlight DashboardHeader guifg=#ffffff]])
+vim.g.dashboard_default_executive = "telescope"
+vim.g.dashboard_custom_section = {
+    a = {
+        description = { "  New File                           " },
+        command = "enew",
+    },
+    b = {
+        description = { "  Competitive                        " },
+        command = "CphReceive",
+    },
+    c = {
+        description = { "  Find Files                         " },
+        command = "Telescope find_files",
+    },
+}
+
