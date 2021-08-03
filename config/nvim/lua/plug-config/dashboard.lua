289ce2625f4a44f74e2bbd3887a6d1624868f402
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-08-03 13:11:04 +0530

    Added footer to dashboard

--- config/nvim/lua/plug-config/dashboard.lua
+++ config/nvim/lua/plug-config/dashboard.lua
@@ -17,7 +17,9 @@ vim.g.dashboard_custom_header = {
     "                                                              "
 }
 vim.cmd([[highlight DashboardHeader guifg=#ffffff]])
+
 vim.g.dashboard_default_executive = "telescope"
+
 vim.g.dashboard_custom_section = {
     a = {
         description = { "  New File                       SPC n" },
@@ -35,5 +37,13 @@ vim.g.dashboard_custom_section = {
         description = { "  Find History                   SPC o" },
         command = "Telescope oldfiles",
     },
+}
+
+local plugins_count = vim.fn.len(
+    vim.fn.globpath("~/.local/share/nvim/site/pack/packer/start", "*", 0, 1)
+)
+
+vim.g.dashboard_custom_footer = {
+    "-- Neovim Loaded " .. plugins_count .. " Plugins --",
 }
 
