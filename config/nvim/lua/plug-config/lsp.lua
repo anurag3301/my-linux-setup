b6a25d0af69879a5338e39965c1a58d8fec31a53
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-08-04 19:40:55 +0530

    Added New Icon pack to lsp-compe

--- config/nvim/lua/plug-config/lsp.lua
+++ config/nvim/lua/plug-config/lsp.lua
@@ -96,3 +96,51 @@ vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 
+vim.lsp.protocol.CompletionItemKind = {
+    "   (Text) ",
+    "   (Method)",
+    "   (Function)",
+    "   (Constructor)",
+    " ﴲ  (Field)",
+    "[] (Variable)",
+    "   (Class)",
+    " ﰮ  (Interface)",
+    "   (Module)",
+    " 襁 (Property)",
+    "   (Unit)",
+    "   (Value)",
+    " 練 (Enum)",
+    "   (Keyword)",
+    "   (Snippet)",
+    "   (Color)",
+    "   (File)",
+    "   (Reference)",
+    "   (Folder)",
+    "   (EnumMember)",
+    " ﲀ  (Constant)",
+    " ﳤ  (Struct)",
+    "   (Event)",
+    "   (Operator)",
+    "   (TypeParameter)",
+}
+
+vim.fn.sign_define(
+    "LspDiagnosticsSignError",
+    { texthl = "LspDiagnosticsSignError", text = "", numhl = "LspDiagnosticsSignError" }
+)
+vim.fn.sign_define(
+    "LspDiagnosticsSignWarning",
+    { texthl = "LspDiagnosticsSignWarning", text = "", numhl = "LspDiagnosticsSignWarning" }
+)
+vim.fn.sign_define(
+    "LspDiagnosticsSignHint",
+    { texthl = "LspDiagnosticsSignHint", text = "", numhl = "LspDiagnosticsSignHint" }
+)
+vim.fn.sign_define(
+    "LspDiagnosticsSignInformation",
+    {
+        texthl = "LspDiagnosticsSignInformation",
+        text = "",
+        numhl = "LspDiagnosticsSignInformation",
+    }
+)
