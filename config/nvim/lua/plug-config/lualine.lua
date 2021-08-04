42631a134921330c2bd2f5b257a2278c8f9f049d
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-08-04 18:50:31 +0530

    Added lualine defalult config

--- config/nvim/lua/plug-config/lualine.lua
+++ config/nvim/lua/plug-config/lualine.lua
@@ -1,6 +1,28 @@
 -- include lualine and its config
-require('lualine').setup{
-	  options = {
-      theme = 'material-nvim'
-  }
+require'lualine'.setup {
+  options = {
+    icons_enabled = true,
+    theme = 'material-nvim',
+    component_separators = {'', ''},
+    section_separators = {'', ''},
+    disabled_filetypes = {}
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
 }
