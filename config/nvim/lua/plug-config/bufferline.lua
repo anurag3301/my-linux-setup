06aeef0bd4a94284fa8c44e2e7590fbbd62b0ec8
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-08-06 11:30:13 +0530

    Added my config for nvim-bufferline

--- config/nvim/lua/plug-config/bufferline.lua
+++ config/nvim/lua/plug-config/bufferline.lua
@@ -1,7 +1,7 @@
 require('bufferline').setup {
       options = {
-        numbers = "both",
-        number_style = "superscript",
+        numbers = "ordinal",
+        number_style = "",
         mappings = false,
         indicator_icon = "▎",
         buffer_close_icon = "",
@@ -10,22 +10,23 @@ require('bufferline').setup {
         left_trunc_marker = "",
         right_trunc_marker = "",
         max_name_length = 18,
-        max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+        max_prefix_length = 15,
         tab_size = 18,
         diagnostics = "nvim_lsp",
         diagnostics_indicator = function(count, level, diagnostics_dict, context)
             return "(" .. count .. ")"
         end,
-        offsets = { { filetype = "NvimTree", text_align = "left" } },
+        offsets = { { filetype = "nerdtree", text_align = "left" } },
         show_buffer_icons = true,
         show_buffer_close_icons = false,
         show_close_icon = false,
         show_tab_indicators = true,
-        persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+        persist_buffer_sort = true,
         enforce_regular_tabs = false,
         always_show_bufferline = true,
         sort_by = "id",
     },
+--[[
     highlights = {
         background = {
             guibg = "#3B4252",
@@ -78,6 +79,6 @@ require('bufferline').setup {
             guifg = "#5E687B",
         },
     },
-
+    ]]
 }
 
