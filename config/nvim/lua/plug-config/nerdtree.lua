a615530a8901bdbbeb5faf4135c027296c38324a
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-08-17 18:58:08 +0530

    Removed Nred tree minimal ui

--- config/nvim/lua/plug-config/nerdtree.lua
+++ config/nvim/lua/plug-config/nerdtree.lua
@@ -4,7 +4,6 @@ vim.g.NERDTreeExactMatchHighlightFullName = 1
 vim.g.NERDTreePatternMatchHighlightFullName = 1
 vim.api.nvim_set_keymap('n', '<F7>', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
 vim.g.NERDTreeQuitOnOpen = 1
-vim.g.NERDTreeMinimalUI = 1
 vim.g.NERDTreeDirArrows = 1
 vim.g.NERDTreeGitStatusUseNerdFonts = 1
 vim.cmd([[
