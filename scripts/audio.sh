d586d09c7ca3501cdc236f670cb970263229f318
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-09-15 19:18:48 +0530

    Update audio output device change script

--- scripts/audio.sh
+++ scripts/audio.sh
@@ -5,7 +5,7 @@ var=$(pacmd list-sinks | grep -e 'index:' | awk '{print $1}' | head -n 1)
 if [ "$var" = "*" ]
 then
     echo "Its Headphone, changing to speakers"
-    pacmd "set-default-sink alsa_output.pci-0000_05_00.1.hdmi-stereo"
+    pacmd "set-default-sink alsa_output.pci-0000_05_00.1.hdmi-stereo-extra2"
 else
     echo "Its speakers, changing to Headphone"
     pacmd "set-default-sink alsa_output.pci-0000_07_00.4.analog-stereo"
