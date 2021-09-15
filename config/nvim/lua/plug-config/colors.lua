59f3e1c2bb5811c08893cd78114b301a135a5b1e
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-09-15 12:28:07 +0530

    -

--- config/nvim/lua/plug-config/colors.lua
+++ config/nvim/lua/plug-config/colors.lua
@@ -7,7 +7,7 @@ vim.g.material_italic_variables = true
 vim.g.material_contrast = true
 vim.g.material_borders = true
 vim.g.material_disable_background = false
-require('material').set()
+vim.cmd("colorscheme material")
 vim.api.nvim_set_keymap('n', '<leader>mm', [[<Cmd>lua require('material.functions').toggle_style()<CR>]], { noremap = true, silent = true })
 
 
