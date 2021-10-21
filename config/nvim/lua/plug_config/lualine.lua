e275c1c50f5cbad5d4e3ad2ffaa78b20fbf4116a
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-10-21 14:51:23 +0530

    Added lualine

---
+++ config/nvim/lua/plug_config/lualine.lua
@@ -0,0 +1,25 @@
+require'lualine'.setup {
+  options = {
+    icons_enabled = true,
+    theme = 'material-nvim',
+    component_separators = {'', ''},
+    section_separators = {'', ''},
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+      lualine_y = {
+        {
+          "diagnostics",
+          sources = { "nvim_lsp" },
+          color_error = "#BF616A",
+          color_warn = "#EBCB8B",
+          color_info = "#88C0D0",
+          color_hint = "#3A4151",
+        },
+      },
+    lualine_z = {'location'}
+  },
+}
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -2,5 +2,9 @@ return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 	use 'marko-cerovac/material.nvim'
+	use {
+		'hoob3rt/lualine.nvim',
+		requires = {'kyazdani42/nvim-web-devicons', opt = true}
+	  }	  
 
 end)
