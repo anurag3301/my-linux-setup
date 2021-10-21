309df86c248de7bfe12450296daf22f7e9c657a4
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-10-21 13:57:32 +0530

    Added Treesitter

--- config/nvim/lua/plugin.lua
+++ config/nvim/lua/plugin.lua
@@ -1,4 +1,5 @@
-return require('packer').startup(function()
+return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
 end)
