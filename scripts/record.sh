3359038fc4ce4ec6618a7c3d722be1e1a19a1f70
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-10-26 17:02:37 +0530

    added spaces

--- scripts/record.sh
+++ scripts/record.sh
@@ -6,10 +6,12 @@ case "$ARG" in
     echo "Recording Desktop audio" &
     ffmpeg -y -f pulse -i $(pactl get-default-sink).monitor\
       "$HOME/vid/desktop_$(date '+%d-%m-%y-%H:%M:%S').wav";;
+
   "m")
     echo "Recording Mircrophone audio" &
     ffmpeg -y -f pulse -i alsa_input.usb-DCMT_Technology_USB_Condenser_Microphone_214b206000000178-00.mono-fallback\
       "$HOME/vid/mictrophone_$(date '+%d-%m-%y-%H:%M:%S').wav";;
+
   "s1") 
     echo "Recording with screen 1" & 
     ffmpeg -y -f x11grab -r 25 -s $(xrandr | fgrep '*' | awk '{print $1}')\
@@ -17,6 +19,7 @@ case "$ARG" in
       "$HOME/vid/record_$(date '+%d-%m-%y-%H:%M:%S').mkv";;
   
   "s2") echo "Recording with screen 2" ;;
+
   "s1m") echo "Recording with screen 1 and microphone" &
     ffmpeg -y -f pulse -i alsa_input.usb-DCMT_Technology_USB_Condenser_Microphone_214b206000000178-00.mono-fallback\
       -f x11grab -r 25 -s $(xrandr | fgrep '*' | awk '{print $1}')\
@@ -24,6 +27,7 @@ case "$ARG" in
       "$HOME/vid/record_$(date '+%d-%m-%y-%H:%M:%S').mkv";;
 
   "s2m") echo "Recording with screen 2 and microphone" ;;
+
   "s1d") echo "Recording with screen 1 and desktop audio" &
     ffmpeg -y -f pulse -i $(pactl get-default-sink).monitor\
       -f x11grab -r 25 -s $(xrandr | fgrep '*' | awk '{print $1}')\
@@ -31,7 +35,9 @@ case "$ARG" in
       "$HOME/vid/record_$(date '+%d-%m-%y-%H:%M:%S').mkv";;
   
   "s2d") echo "Recording with screen 2 and desktop audio" ;;
+
   "s1dm") echo "Recording with screen 1 and both microphone and desktop audio" ;;
+  
   "s2dm") echo "Recording with screen 2 and both microphone and desktop audio" ;;
 esac
 
