433142fc82d2415e4c3893edd2f593bbbaa8c439
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-10-31 13:57:42 +0530

    Fixed lualine bug

--- config/nvim/lua/plug_config/lualine.lua
+++ config/nvim/lua/plug_config/lualine.lua
@@ -2,8 +2,6 @@ require'lualine'.setup {
   options = {
     icons_enabled = true,
     theme = 'material-nvim',
-    component_separators = {'', ''},
-    section_separators = {'', ''},
   },
   sections = {
     lualine_a = {'mode'},
