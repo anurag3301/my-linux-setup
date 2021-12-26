fe59571cdf4b5da08ba3b010f3e6569a660e0488
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-12-26 18:30:31 +0530

    Added script for wacom tablet

---
+++ scripts/wacom.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+id="$(xsetwacom list devices | grep stylus | awk '{print $9}')"
+
+if [ "$1" == "l" ] 
+then
+    xsetwacom set $id MapToOutput 1366x768+0+0
+
+elif [ "$1" == "r" ] 
+then
+    xsetwacom set $id MapToOutput 1366x768+1366+0
+
+else
+    echo "Invalid input"
+fi
+
