c75e7f17c60db58489bf768fb3c0281a997060a1
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-01-01 18:38:59 +0530

    Added loggin for 0x0.st upload script

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -2,7 +2,13 @@
 
 command="curl -s -F'file=@$(pwd)/$1' https://0x0.st"
 echo "Executing: $command"
+
 url=$(eval $command)
 echo "URL: $url"
+
 printf "$url" | xclip -sel clip
 notify-send "File Uploaded" "URL: $url \ncopied to clipboard" -t 5000 -i "$HOME/.program/icon.png"
+
+entry="$(date '+%d-%m-%y-%H:%M:%S')    $url    $(echo $1 | awk -F'/' '{print $(NF)}')"
+echo $entry >> $HOME/.0x0_list
+
