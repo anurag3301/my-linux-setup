909b09301795ece7642ee155c901751c20b6ddfd
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-01-09 12:10:02 +0530

    Fixed tab in plugin.lua

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,75 +1,75 @@
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-	-- apperative plugins
-	use 'hrsh7th/vim-vsnip'
-	use 'glepnir/dashboard-nvim'
-	use 'marko-cerovac/material.nvim'
-	use 'andweeb/presence.nvim'
+    -- apperative plugins
+    use 'hrsh7th/vim-vsnip'
+    use 'glepnir/dashboard-nvim'
+    use 'marko-cerovac/material.nvim'
+    use 'andweeb/presence.nvim'
   
-	-- functional plugins
-	use 'neovim/nvim-lspconfig'
-	use 'hrsh7th/nvim-compe'
+    -- functional plugins
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/nvim-compe'
     use 'hrsh7th/nvim-cmp'
     use 'hrsh7th/cmp-nvim-lsp'
-	use 'williamboman/nvim-lsp-installer'
+    use 'williamboman/nvim-lsp-installer'
     use 'onsails/lspkind-nvim'
-	use 'nvim-treesitter/nvim-treesitter'
+    use 'nvim-treesitter/nvim-treesitter'
     use 'nvim-treesitter/nvim-treesitter-refactor'
     use 'p00f/nvim-ts-rainbow'
-	use 'akinsho/nvim-toggleterm.lua'
-	use 'windwp/nvim-autopairs'
-	use 'windwp/nvim-ts-autotag'
-	use 'norcalli/nvim-colorizer.lua'
-	use 'WhoIsSethDaniel/toggle-lsp-diagnostics.nvim'
-	use 'kdheepak/lazygit.nvim'
-	use 'terrortylor/nvim-comment'
-	use "rafamadriz/friendly-snippets"
-	use "Pocco81/AutoSave.nvim"
-	use "jbyuki/venn.nvim"
-	use "turbio/bracey.vim"
+    use 'akinsho/nvim-toggleterm.lua'
+    use 'windwp/nvim-autopairs'
+    use 'windwp/nvim-ts-autotag'
+    use 'norcalli/nvim-colorizer.lua'
+    use 'WhoIsSethDaniel/toggle-lsp-diagnostics.nvim'
+    use 'kdheepak/lazygit.nvim'
+    use 'terrortylor/nvim-comment'
+    use "rafamadriz/friendly-snippets"
+    use "Pocco81/AutoSave.nvim"
+    use "jbyuki/venn.nvim"
+    use "turbio/bracey.vim"
     use "iamcco/markdown-preview.nvim"
     use "rcarriga/nvim-notify"
   
-	use {
-	  'lewis6991/gitsigns.nvim',
-	  requires = {
-		'nvim-lua/plenary.nvim'
-	  }
-	}
+    use {
+      'lewis6991/gitsigns.nvim',
+      requires = {
+        'nvim-lua/plenary.nvim'
+      }
+    }
   
-	use {
-	'phaazon/hop.nvim',
-	as = 'hop'
-	}
+    use {
+    'phaazon/hop.nvim',
+    as = 'hop'
+    }
   
-	use {
-	  'akinsho/nvim-bufferline.lua',
-	  requires = 'kyazdani42/nvim-web-devicons'
-	}
+    use {
+      'akinsho/nvim-bufferline.lua',
+      requires = 'kyazdani42/nvim-web-devicons'
+    }
   
-	use {
-	  'p00f/cphelper.nvim',
-	  rocks = 'http',
-	  requires = {
-		'nvim-lua/plenary.nvim',
-		'ygm2/rooter.nvim',
-		opt = true
-	  }
-	}
+    use {
+      'p00f/cphelper.nvim',
+      rocks = 'http',
+      requires = {
+        'nvim-lua/plenary.nvim',
+        'ygm2/rooter.nvim',
+        opt = true
+      }
+    }
   
-	use {
-	  'hoob3rt/lualine.nvim',
-	  requires = {
-		'kyazdani42/nvim-web-devicons',
-		opt = true
-	  }
-	}
+    use {
+      'hoob3rt/lualine.nvim',
+      requires = {
+        'kyazdani42/nvim-web-devicons',
+        opt = true
+      }
+    }
   
-	use {
-	  'nvim-telescope/telescope.nvim',
-	  requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
-	}
+    use {
+      'nvim-telescope/telescope.nvim',
+      requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+    }
 
   use {
     'kyazdani42/nvim-tree.lua',
