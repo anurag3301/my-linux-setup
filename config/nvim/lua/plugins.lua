c59f44a5c4fe0faac5adf32a17b4191bb3d1bfbd
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-01-09 12:10:33 +0530

    Installed sniprun

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -16,6 +16,7 @@ return require('packer').startup(function(use)
     use 'onsails/lspkind-nvim'
     use 'nvim-treesitter/nvim-treesitter'
     use 'nvim-treesitter/nvim-treesitter-refactor'
+    use {'michaelb/sniprun', run = 'bash ./install.sh'}
     use 'p00f/nvim-ts-rainbow'
     use 'akinsho/nvim-toggleterm.lua'
     use 'windwp/nvim-autopairs'
