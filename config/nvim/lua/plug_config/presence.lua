0d3c2ea803461805d35e423652b8b18150679223
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-03-26 17:52:37 +0530

    Set number line show to false in nvim.presence

--- config/nvim/lua/plug_config/presence.lua
+++ config/nvim/lua/plug_config/presence.lua
@@ -1,6 +1,6 @@
 -- include presence and its config
 require("presence"):setup({
-    enable_line_number = true,
+    enable_line_number = false,
     main_image = "file",
     neovim_image_text = "Its Neovim buddy!!!",
     debounce_timeout = 10,
