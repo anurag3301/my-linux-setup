ea0b0e6478bc6807a7b510cde58cfad856997811
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-04-24 18:51:34 +0530

    commented out fzf extenstion

--- config/nvim/lua/plug_config/telescope.lua
+++ config/nvim/lua/plug_config/telescope.lua
@@ -17,14 +17,14 @@ require('telescope').setup({
 			},
 		},
 	},
-	extensions = {
-		fzy = {
-            fuzzy = true,
-			override_generic_sorter = false,
-			override_file_sorter = true,
-            case_mode = 'smart_case',
-		},
-	},
+	-- extensions = {
+	-- 	fzy = {
+	--             fuzzy = true,
+	-- 		override_generic_sorter = false,
+	-- 		override_file_sorter = true,
+	--             case_mode = 'smart_case',
+	-- 	},
+	-- },
 })
 
-require('telescope').load_extension('fzf')
+-- require('telescope').load_extension('fzf')
