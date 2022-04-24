62ea6915be19fb5b437d1c09024644d083d1e39c
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-04-24 18:51:01 +0530

    Removed cmp-cmdline

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -12,7 +12,6 @@ return require('packer').startup(function(use)
     use 'hrsh7th/cmp-nvim-lsp'
     use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
     use 'hrsh7th/nvim-cmp'
     use 'williamboman/nvim-lsp-installer'
     use 'onsails/lspkind-nvim'
