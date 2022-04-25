10ad4399052a5bac213312eb1435f09473ce8357
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-04-25 13:55:42 +0530

    Updated the layout of code_runner

--- config/nvim/lua/runner.lua
+++ config/nvim/lua/runner.lua
@@ -1,6 +1,12 @@
+local run_command_table = {
+    ['cpp'] = 'g++ %:t -o %:r && ./%:r',
+}
+
 function run_code()
-    if (vim.bo.filetype == 'cpp')
-    then
-       vim.cmd('2TermExec cmd="timeout 4 g++ -std=c++17 %:t -o %:r && ./%:r" direction=float')
-    end
+    if (run_command_table[vim.bo.filetype]) then
+       vim.cmd('2TermExec cmd="'.. run_command_table[vim.bo.filetype]..'" direction=float')
+   else
+       print("\nFileType not supported\n")
+   end
 end
+
