6dee09b32e8474ffd2d8b2a38e8d23c78f9a8073
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-04-26 08:09:30 +0530

    Updated the code runner script with autoclose after execution

--- config/nvim/lua/runner.lua
+++ config/nvim/lua/runner.lua
@@ -10,9 +10,11 @@ local run_command_table = {
     ['go'] = 'go run %:t'
 }
 
+local extra = 'echo \"\\\\n\\\\033[0;33mPlease Press ENTER to continue \\\\033[0m\"; read; exit;'
+
 function run_code()
     if (run_command_table[vim.bo.filetype]) then
-       vim.cmd('2TermExec cmd="'.. run_command_table[vim.bo.filetype]..'" direction=float')
+       vim.cmd("2TermExec cmd='".. run_command_table[vim.bo.filetype].."; " .. extra .. "' direction=float")
    else
        print("\nFileType not supported\n")
    end
