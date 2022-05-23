802d4e50a1651d98ec59576a653717e49508162a
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-05-23 18:57:11 +0530

    nvim: added markdown, make, json and vim to treesitter ensure_install

--- config/nvim/lua/plug_config/treesitter.lua
+++ config/nvim/lua/plug_config/treesitter.lua
@@ -3,7 +3,8 @@ require('nvim-treesitter.configs').setup{
 
   ensure_installed = {'c', 'cpp', 'python', 'lua', 'typescript',
                       'regex', 'bash', 'cmake', 'css', 'javascript',
-                      'html', 'comment', 'java', 'rust', 'go'},
+                      'html', 'comment', 'java', 'rust', 'go', 'markdown',
+                      'make', 'json', 'vim'},
   highlight = {
     enable = true,
     disable = {},
