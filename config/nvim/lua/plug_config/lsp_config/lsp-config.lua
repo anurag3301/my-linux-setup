b89b28a9fd7df2e791c5896601a9eb9569b0fac4
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-06-10 10:27:46 +0530

    Added markdown lsp-server: marksman

--- config/nvim/lua/plug_config/lsp_config/lsp-config.lua
+++ config/nvim/lua/plug_config/lsp_config/lsp-config.lua
@@ -39,7 +39,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-local servers = {'pyright', 'arduino_language_server', 'tsserver', 'hls', 'cmake', 'html', 'cssls', 'rust_analyzer', 'sumneko_lua', 'bashls', 'ccls'}
+local servers = {'pyright', 'arduino_language_server', 'tsserver', 'hls', 'cmake', 'html', 'cssls', 'rust_analyzer', 'sumneko_lua', 'bashls', 'ccls', 'marksman'}
 
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
