ce8392dcc2ebff7fe4b22138f18a08c5c881ef7b
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-08-12 22:38:03 +0530

    Changed python lsp server from pyright to pylsp

--- config/nvim/lua/plug_config/lsp_config/lsp-config.lua
+++ config/nvim/lua/plug_config/lsp_config/lsp-config.lua
@@ -11,7 +11,7 @@ local nvim_lsp = require('lspconfig')
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-local servers = {'pyright', 'arduino_language_server', 'tsserver', 'hls', 'cmake', 'html', 'cssls', 'rust_analyzer', 'sumneko_lua', 'bashls', 'ccls', 'marksman'}
+local servers = {'pylsp', 'arduino_language_server', 'tsserver', 'hls', 'cmake', 'html', 'cssls', 'rust_analyzer', 'sumneko_lua', 'bashls', 'ccls', 'marksman'}
 
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
