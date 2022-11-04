60b620845790edb018ff540b983dd08772e722e3
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2022-11-04 22:14:49 +0530

    Added the custom config for material.nvim

--- config/nvim/lua/plug_config/colour.lua
+++ config/nvim/lua/plug_config/colour.lua
@@ -4,64 +4,68 @@ vim.g.material_style = 'deep ocean'
 require('material').setup({
 
     contrast = {
-        terminal = false, -- Enable contrast for the built-in terminal
-        sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
-        floating_windows = false, -- Enable contrast for floating windows
-        cursor_line = false, -- Enable darker background for the cursor line
-        non_current_windows = false, -- Enable darker background for non-current windows
-        filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
+        terminal = false,
+        sidebars = true,
+        floating_windows = true,
+        cursor_line = false,
+        non_current_windows = false,
+        filetypes = {
+            'terminal',
+            'packer',
+            'qf'
+        },
     },
 
-    styles = { -- Give comments style such as bold, italic, underline etc.
-        comments = { --[[ italic = true ]] },
-        strings = { --[[ bold = true ]] },
-        keywords = { --[[ underline = true ]] },
-        functions = { --[[ bold = true, undercurl = true ]] },
+    styles = {
+        comments = { italic = true },
+        strings = {},
+        keywords = {},
+        functions = { italic = true },
         variables = {},
         operators = {},
-        types = {},
+        types = { bold = true },
     },
 
-    plugins = { -- Uncomment the plugins that you use to highlight them
-        -- Available plugins:
+    plugins = {
         -- "dap",
-        -- "dashboard",
-        -- "gitsigns",
-        -- "hop",
+        "dashboard",
+        "gitsigns",
+        "hop",
         -- "indent-blankline",
         -- "lspsaga",
         -- "mini",
         -- "neogit",
-        -- "nvim-cmp",
+        "nvim-cmp",
         -- "nvim-navic",
-        -- "nvim-tree",
+        "nvim-tree",
         -- "sneak",
-        -- "telescope",
+        "telescope",
         -- "trouble",
         -- "which-key",
     },
 
     disable = {
-        colored_cursor = false, -- Disable the colored cursor
-        borders = false, -- Disable borders between verticaly split windows
-        background = false, -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
-        term_colors = false, -- Prevent the theme from setting terminal colors
-        eob_lines = false -- Hide the end-of-buffer lines
+        colored_cursor = false,
+        borders = false,
+        background = false,
+        term_colors = false,
+        eob_lines = false
     },
 
     high_visibility = {
-        lighter = false, -- Enable higher contrast text for lighter style
-        darker = false -- Enable higher contrast text for darker style
+        lighter = false,
+        darker = false
     },
 
-    lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
+    lualine_style = "default",
 
-    async_loading = true, -- Load parts of the theme asyncronously for faster startup (turned on by default)
+    async_loading = true,
 
-    custom_colors = nil, -- If you want to everride the default colors, set this to a function
+    custom_colors = nil,
 
-    custom_highlights = {}, -- Overwrite highlights with your own
+    custom_highlights = {},
 })
+
 vim.cmd 'colorscheme material'
 
 vim.cmd("highlight WinSeparator guifg=#c0bfbc")
