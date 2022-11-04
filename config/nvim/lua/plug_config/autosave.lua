babcb1cd27f532c99dfec0396fe1e5f7a41c5df6
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2022-11-04 22:30:00 +0530

    Fixed auto-save deprications

--- config/nvim/lua/plug_config/autosave.lua
+++ config/nvim/lua/plug_config/autosave.lua
@@ -1,17 +1,34 @@
-local autosave = require('autosave')
+local autosave = require('auto-save')
 
-autosave.setup(
-    {
-        enabled = true,
-        execution_message = 'AutoSave: saved at ' .. vim.fn.strftime('%H:%M:%S'),
-        events = {'InsertLeave', 'TextChanged'},
-        conditions = {
-            exists = true,
-            modifiable = true
-        },
-        write_all_buffers = false,
-        on_off_commands = true,
-        clean_command_line_interval = 0,
-        debounce_delay = 135
-    }
-)
+autosave.setup{
+    enabled = true,
+    execution_message = {
+		message = function()
+			return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+		end,
+		dim = 0.18,
+		cleaning_interval = 1250,
+	},
+    trigger_events = {"InsertLeave", "TextChanged"},
+	condition = function(buf)
+		local fn = vim.fn
+		local utils = require("auto-save.utils.data")
+
+		if
+			fn.getbufvar(buf, "&modifiable") == 1 and
+			utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+			return true
+		end
+		return false
+	end,
+    write_all_buffers = false,
+    debounce_delay = 135,
+	callbacks = {
+		enabling = nil,
+		disabling = nil,
+		before_asserting_save = nil,
+		before_saving = nil,
+		after_saving = nil
+	}
+}
+
