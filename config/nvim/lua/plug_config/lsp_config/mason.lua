ec2e0dccf940f6b9d6710406a94b6eff4ecb733b
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2023-11-13 15:16:56 +0530

    Updated lsp stuff

--- config/nvim/lua/plug_config/lsp_config/mason.lua
+++ config/nvim/lua/plug_config/lsp_config/mason.lua
@@ -1,4 +1,4 @@
 require("mason").setup()
 require("mason-lspconfig").setup {
-    ensure_installed = { 'pylsp', 'tsserver', 'cmake', 'html', 'rust_analyzer', 'lua_ls', 'bashls', 'marksman', 'phpactor', 'asm_lsp'},
+    ensure_installed = { 'pylsp', 'tsserver', 'cmake', 'html', 'rust_analyzer', 'lua_ls', 'bashls', 'marksman', 'asm_lsp'},
 }
