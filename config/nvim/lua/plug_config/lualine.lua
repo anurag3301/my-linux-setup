d8f72dc9dd946f78237c35ebdf9184998f5a6cf0
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2024-01-17 16:01:41 +0530

    Neovim: Added git diff notation for lualine_b

--- config/nvim/lua/plug_config/lualine.lua
+++ config/nvim/lua/plug_config/lualine.lua
@@ -5,7 +5,7 @@ require'lualine'.setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch'},
+    lualine_b = {'branch', 'diff'},
     lualine_c = {'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
       lualine_y = {
