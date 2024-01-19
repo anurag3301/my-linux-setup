ca70879a6ad54dcd81f21a44651c4264efdad97e
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2024-01-19 20:49:11 +0530

    Noevim: updated floating_window_y attrib in lsp_signature

--- config/nvim/lua/plug_config/lsp_config/lsp-signature.lua
+++ config/nvim/lua/plug_config/lsp_config/lsp-signature.lua
@@ -2,9 +2,11 @@
 -- Plugin's github: https://github.com/ray-x/lsp_signature.nvim
 
 local config = {
-   doc_lines = 0,
-   hint_enable = false,
-   shadow_guibg = 'Black',
+    doc_lines = 0,
+    hint_enable = false,
+    shadow_guibg = 'Black',
+    floating_window_above_cur_line = false,
+    floating_window_off_y = 2,
 }
 
 require "lsp_signature".setup(config)
