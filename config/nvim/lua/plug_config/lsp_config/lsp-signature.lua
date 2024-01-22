73b8674385565c305c2624b834b1402deafdd2a6
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2024-01-22 23:23:34 +0530

    Neovim: GitSignature floating window x offest update

--- config/nvim/lua/plug_config/lsp_config/lsp-signature.lua
+++ config/nvim/lua/plug_config/lsp_config/lsp-signature.lua
@@ -6,7 +6,7 @@ local config = {
     hint_enable = false,
     shadow_guibg = 'Black',
     floating_window_above_cur_line = false,
-    floating_window_off_y = 2,
+    floating_window_off_y = -2,
 }
 
 require "lsp_signature".setup(config)
