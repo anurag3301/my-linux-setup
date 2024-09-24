e122aecf2d382352252f1ffff7c83a817406c22a
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2024-09-24 10:38:33 +0530

    Updated 4chan script to have configurable board

--- scripts/4chan.sh
+++ scripts/4chan.sh
@@ -7,18 +7,22 @@ then
     exit
 fi
 
-thread_code=$1
-dir=$2
+board=$1
+thread_code=$2
+dir=$3
 
 base_dir="$HOME/.chan"
-complete_dir="$base_dir/$dir"
+complete_dir="$base_dir/$board/$dir"
+echo $complete_dir
 
-base_url="https://boards.4chan.org/gif/thread"
-complete_url="$base_url/$thread_code"
+base_url="https://boards.4chan.org"
+complete_url="$base_url/$board/thread/$thread_code"
+echo $complete_url
 
 mkdir -p $complete_dir
 cd $complete_dir
 
-curl $complete_url | grep -Po '(?<=href=")[^"]*.webm' | awk '{ print "https:"$1 }' | uniq | xargs wget -nc -T 3
+
+curl $complete_url | grep -Po '(?<=href=")[^"]*\.(webm|jpg|png)' | awk '{ print "https:"$1 }' | uniq | xargs wget -nc -T 3
 
 echo "\n!! DOWNLOAD COMPLETED !!"
